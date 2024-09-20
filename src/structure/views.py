aca7905613d39f6bce8c1afe75d204a1facfee7d
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-09-20 21:37:23 +0500

    category api

--- src/structure/views.py
+++ src/structure/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -13,14 +15,75 @@ class CategoryListAPIView(APIView):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
+    # def post(self, request, *args, **kwargs):
+    #     if request.user:
+    #         data = request.data
+    #         # data = {
+    #         #     'title': request.data.get('title'),
+    #         #     'slug': request.data.get('slug'),
+    #         #     'user': request.user.id
+    #         # }
+    #         serializer = CategorySerializer(data=data)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     return Response({'error': 'User must be authenticated'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoryCreateAPI(CreateAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get_object(self, category_id):
+
+        try:
+            return Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            return None
+
+    def get(self, request, category_id, *args, **kwargs):
+
+        category = self.get_object(category_id)
+        if not category:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, category_id, *args, **kwargs):
+
+        category = self.get_object(category_id)
+        if not category:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         data = request.data
-        serializer = CategorySerializer(data=data)
+        serializer = CategorySerializer(instance=category, data=data, partial=True)
         if serializer.is_valid():
             serializer.save()
-
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(
+                serializer.data, status=status.HTTP_200_OK
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
+    def delete(self, request, category_id, *args, **kwargs):
+        category_instance = self.get_object(category_id)
+        if not category_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        category_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
