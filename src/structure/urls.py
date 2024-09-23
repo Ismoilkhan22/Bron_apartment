2c8b99d4fd28c22106dfd3d6c236a72940102090
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-09-23 17:41:50 +0500

    pr_rule api

--- src/structure/urls.py
+++ src/structure/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from src.structure.views import *
 
 urlpatterns = [
+    # category api  urls
     path("category/", CategoryListAPIView.as_view()),
     path("category/create/", CategoryCreateAPI.as_view()),
     path("category/<int:category_id>/", CategoryDetailView.as_view()),
+
+    # product_rule api urls
+    path("product_rule/", ProductRuleListAPIView.as_view()),
+    path("product_rule/<int:pk>/", ProductRuleDetailAPIView.as_view())
+
 ]
